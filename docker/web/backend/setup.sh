1120a723c18e0f0924c47f8410b6751fce21623f
andreadanzi <andrea@danzi.tn.it>
2017-03-12 10:38:58 +0100

    php configuration

--- docker/web/backend/setup.sh
+++ docker/web/backend/setup.sh
@@ -4,9 +4,9 @@ a2enmod rewrite expires
 
 apt-get update 
 
-apt-get install -y libpng12-dev libjpeg-dev libkrb5-dev vim ssh sendmail \
+apt-get install -y libpng12-dev libjpeg-dev libkrb5-dev vim ssh sendmail libmcrypt-dev libzip-dev \
     && docker-php-ext-configure gd --with-png-dir=/usr --with-jpeg-dir=/usr \
-    && docker-php-ext-install gd mysqli \
+    && docker-php-ext-install gd mysqli mcrypt zip \
     && apt-get -y install libssl-dev libc-client2007e-dev libkrb5-dev \
     && docker-php-ext-configure imap --with-imap-ssl --with-kerberos \
     && docker-php-ext-install imap opcache \
@@ -15,3 +15,6 @@ apt-get install -y libpng12-dev libjpeg-dev libkrb5-dev vim ssh sendmail \
 apt-get update
 
 apt-get install -y cron
+
+#php-mcrypt
+#php-zip
